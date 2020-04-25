5f3e341e3da8d4530455fd406ddd0d6631751554
rafi9898 <r-podraza@wp.pl>
2020-04-25 15:49:25 +0200

    Added api tests

---
+++ cypress/integration/api/api.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+describe('REST API Test with Cypres', () => {
+	it('API Test - Validate Headers', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+		cy.get('@pokemon')
+			.its('headers')
+			.its('content-type')
+			.should('include', 'application/json; charset=utf-8')
+	})
+
+	it('API Test - Validate Status Code', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+		cy.get('@pokemon').its('status').should('equal', 200)
+	})
+
+	it('API Test - Validation Name Value', () => {
+		cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+		cy.get('@pokemon').its('body').should('include', { name: 'pikachu' })
+	})
+
+	it('API Test - Validate Negative Status Code', () => {
+		cy.request({
+			method: 'GET',
+			url: 'https://pokeapi.co/api/v2/pokemon/1000',
+			failOnStatusCode: false,
+		}).as('pokemon')
+		cy.get('@pokemon').its('status').should('equal', 404)
+	})
+})
